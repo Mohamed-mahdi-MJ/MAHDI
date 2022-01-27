43f23364d35b1b4240196387a6b76c24ff2aac5f
Mohamed-mahdi-MJ <98368591+Mohamed-mahdi-MJ@users.noreply.github.com>
2022-01-27 09:19:58 +0530

    Update HelloWorld.java

--- Projectmahdi/src/eve/HelloWorld.java
+++ Projectmahdi/src/eve/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to programming in Java...");
-
+                 System.out.println("code checking");
 	}
 
 }
